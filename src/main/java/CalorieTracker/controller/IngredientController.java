ee19ed19ad49ca5853ba3a4783fe5476f9594fa8
AntoineMethot <amethot@etu.uqac.ca>
2025-06-25 14:34:15 -0400

    Added controller for thymeleaf templates and ingredient list template

--- src/main/java/CalorieTracker/controller/IngredientController.java
+++ src/main/java/CalorieTracker/controller/IngredientController.java
@@ -3,53 +3,28 @@ package CalorieTracker.controller;
 import CalorieTracker.entity.Ingredient;
 import CalorieTracker.service.IngredientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api")
+@Controller
+@RequestMapping("/ingredients")
 public class IngredientController {
 
-    private final IngredientService ingredientService;
+    private IngredientService ingredientService;
 
     @Autowired
     public IngredientController(IngredientService ingredientService) {
         this.ingredientService = ingredientService;
     }
 
-    @GetMapping("/")
-    public String home() {
-        return "Welcome to the Calorie Tracker API";
-    }
-
-    @GetMapping("/ingredients")
-    public List<Ingredient> findAllIngredients() {
-        return ingredientService.findAll();
-    }
-
-    @GetMapping("/ingredients/{id}")
-    public Ingredient findIngredientById(@PathVariable Long id) {
-        return ingredientService.findById(id);
-    }
-
-    @PostMapping("/ingredients")
-    public Ingredient saveIngredient(@RequestBody Ingredient ingredient) {
-        ingredient.setId(null); // Ensure a new ingredient is created
-        return ingredientService.save(ingredient);
-    }
-
-    @PutMapping("/ingredients/{id}")
-    public Ingredient updateIngredient(@PathVariable Long id, @RequestBody Ingredient ingredient) {
-        // Throws if not found
-        ingredientService.findById(id);
-
-        ingredient.setId(id); // Force correct ID
-        return ingredientService.save(ingredient);
-    }
-
-    @DeleteMapping("/ingredients/{id}")
-    public void deleteIngredient(@PathVariable Long id) {
-        ingredientService.deleteById(id);
+    @GetMapping("/list")
+    public String listIngredients(Model model) {
+        List<Ingredient> ingredients = ingredientService.findAll();
+        model.addAttribute("ingredients", ingredients);
+        return "ingredients/list-ingredients"; // Thymeleaf template path
     }
 }
