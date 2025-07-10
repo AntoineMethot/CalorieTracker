bd53ac0f4d4a4202c7b8cbe3e1c3d17ac920e8e0
AntoineMethot <amethot@etu.uqac.ca>
2025-07-10 14:33:58 -0400

    Starting to create list-user html and modified passwordEncoder to read encoding type prefix in DB

--- src/main/java/CalorieTracker/security/SecurityConfig.java
+++ src/main/java/CalorieTracker/security/SecurityConfig.java
@@ -3,7 +3,7 @@ package CalorieTracker.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
@@ -22,7 +22,7 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     @Bean
