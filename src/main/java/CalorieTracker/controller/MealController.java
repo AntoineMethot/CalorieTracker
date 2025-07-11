efca597039279cc09a7304b12d030e40032a814b
AntoineMethot <amethot@etu.uqac.ca>
2025-07-11 11:06:54 -0400

    feat: creating mealController and creating endpoints for meal list and meal form

---
+++ src/main/java/CalorieTracker/controller/MealController.java
@@ -0,0 +1,52 @@
+package CalorieTracker.controller;
+
+import CalorieTracker.entity.Ingredient;
+import CalorieTracker.entity.Meal;
+import CalorieTracker.service.meal.MealService;
+import CalorieTracker.service.mealingredient.MealIngredientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/meals")
+public class MealController {
+
+    private final MealService mealService;
+    private final MealIngredientService mealIngredientService;
+
+    @Autowired
+    public MealController(MealService mealService,
+                          MealIngredientService mealIngredientService) {
+        this.mealService = mealService;
+        this.mealIngredientService = mealIngredientService;
+    }
+
+    @GetMapping("/list")
+    public String listMeals(Model model) {
+        // Logic to retrieve and display meals
+        // Get the current authenticated user's username
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUsername = authentication.getName();
+
+        // Find list of meals for the current user
+        List<Meal> meals = mealService.findByUsername(currentUsername);
+        model.addAttribute("meals", meals);
+
+        return "meals/list-meals"; // Thymeleaf template path for the meal list page
+    }
+
+    @GetMapping("/addMeal")
+    public String addMeal(Model model) {
+        Meal meal = new Meal(); // Create a new ingredient object
+        model.addAttribute("meal", meal);
+        return "meals/meals-form"; // Thymeleaf template path for the form
+    }
+
+}
